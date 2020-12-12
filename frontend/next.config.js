eb702b53296c3fa0edc92df2cadb5a750ce41aa2
Francisco Cajlon Jhonathan Moura Batista <cajlonbatista@outlook.com.br>
2020-12-12 12:47:26 -0300

    test with next-pwa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,2 +1,9 @@
-const withImages = require('next-images')
-module.exports = withImages()
+const withImages = require('next-images');
+const withPWA = require('next-pwa');
+
+module.exports = withImages();
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  }
+})
